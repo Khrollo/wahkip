5c57d4d15c99c1214b10bef245fef1937fa5c0c9
Khrollo <kejhawn@gmail.com>
2025-10-19 08:11:10 -0500

    Fix dark mode: remove unused import and simplify CSS

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import { useTheme } from "./ThemeProvider";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
 
